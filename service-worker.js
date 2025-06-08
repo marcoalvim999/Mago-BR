22489fa694f067b26a3e1ae0d5e2bb2e45134c56
Marco Alvim <97541090+marcoalvim999@users.noreply.github.com>
2025-06-08 11:49:26 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,12 @@
-self.addEventListener("install", function(e) {
-  e.waitUntil(
-    caches.open("mago-br").then(function(cache) {
-      return cache.addAll([
-        ".",
-        "index.html",
-        "favicon.png",
-        "manifest.json"
-      ]);
-    })
-  );
+self.addEventListener('install', event => {
+  console.log('🧙 Service Worker instalado.');
+  self.skipWaiting(); // ativa imediatamente
 });
-self.addEventListener("fetch", function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+
+self.addEventListener('activate', event => {
+  console.log('🧙 Service Worker ativado.');
+});
+
+self.addEventListener('fetch', event => {
+  // você pode implementar cache futuro aqui
 });
